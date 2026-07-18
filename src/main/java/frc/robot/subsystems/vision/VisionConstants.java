2c1d34a8b293207ea41d5bdd4e89c5f18e962647
Drew Currie <drew.currie@gallatinvalleyrobotics.org>
2026-07-18 16:28:23 -0600

    Fixed Vision Constants with stable pose estimation. Tested at KDays 07-18-26 at 4:30PM working.

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -36,7 +36,7 @@ public class VisionConstants {
       new Transform3d(-0.2936, -0.3084, 0.2145, new Rotation3d(0.0, 0.0, -1.5708));
   // Order of signs: (-,+,+, (0,+,0))
   public static Transform3d backCenterToRobot =
-      new Transform3d(-0.1773428, 0.1070864, 0.4154043, new Rotation3d(0.0, 0.463646716, 3.1415));
+      new Transform3d(-0.1773428, 0.1070864, 0.4154043, new Rotation3d(0.0, -0.4636, 3.1415));
 
   // The below transform does not seem to be part of the vision transforms for estimating robot
   // position.
@@ -63,6 +63,6 @@ public class VisionConstants {
       };
 
   // Multipliers to apply for MegaTag 2 observations
-  public static double linearStdDevMegatag2Factor = 0 ; // More stable than full 3D solve
+  public static double linearStdDevMegatag2Factor = 0; // More stable than full 3D solve
   public static double angularStdDevMegatag2Factor = 0; // No rotation data available
 }
