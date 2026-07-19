413f135f62349d31d11a4cc3e23e4f8b28dbfb98
Drew Currie <drew.currie@gallatinvalleyrobotics.org>
2026-07-18 22:04:20 -0600

    Changed angles of rotation to be positive values.
    Changed ambiguity cutoff threshold to be much lower. This comes from a histogram of the ambaguity during a match.

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -30,10 +30,10 @@ public class VisionConstants {
   // New camera transforms updated 07/18/2026
   // Order of signs: (-,+,+, (+,+,+))
   public static Transform3d backElectronicsCameraToRobot =
-      new Transform3d(-0.2936, 0.3084, 0.2145, new Rotation3d(0.0, 0.0, 1.5708));
+      new Transform3d(-0.2936, 0.3084, 0.2145, new Rotation3d(0.0, 0.0, Math.PI/2));
   // Order of signs: (-, -, +, (0,0,-))
   public static Transform3d backTurretCameraToRobot =
-      new Transform3d(-0.2936, -0.3084, 0.2145, new Rotation3d(0.0, 0.0, -1.5708));
+      new Transform3d(-0.2936, -0.3084, 0.2145, new Rotation3d(0.0, 0.0, (3*Math.PI)/2));
   // Order of signs: (-,+,+, (0,+,0))
   public static Transform3d backCenterToRobot =
       new Transform3d(-0.1773428, 0.1070864, 0.4154043, new Rotation3d(0.0, -0.4636, 3.1415));
@@ -45,7 +45,7 @@ public class VisionConstants {
       new Transform3d(0, 2.5, 0.0, new Rotation3d(0.0, -0.2, 0.0));
 
   // Basic filtering thresholds
-  public static double maxAmbiguity = 0.3;
+  public static double maxAmbiguity = 0.225;
   public static double maxZError = 0.75;
 
   // Standard deviation baselines, for 1 meter distance and 1 tag
